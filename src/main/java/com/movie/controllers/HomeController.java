dfc734a279f01dbbf37cd3356e35391e406d8f86
rizwannoorani <rizwannoorani@rizwans-mbp>
2017-02-28 20:35:28 -0500

    added the logic and pages for single sign on test Change-Id: Ifb15da2e64922292d6eef3da3b64fd7b2f131ba3

--- src/main/java/com/movie/controllers/HomeController.java
+++ src/main/java/com/movie/controllers/HomeController.java
@@ -1,22 +1,26 @@
 package com.movie.controllers;
 
+import com.stormpath.sdk.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.stormpath.sdk.application.Application;
- 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-public class MainController {
- 
-	@Autowired
+public class HomeController {
+
+    @Autowired
     Application app;
-	@RequestMapping("/")
-    public String home(Model model) {
+
+    @RequestMapping("/")
+    public String home(HttpServletRequest req, Model model) {
+        model.addAttribute("status", req.getParameter("status"));
+
         model.addAttribute("appName", app.getName());
         model.addAttribute("appDescription", app.getDescription());
- 
+
         return "home";
     }
 }
